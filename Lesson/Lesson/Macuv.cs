3c99455a00a9b73ce165eb7799eba6bd52e644c1
YanaPrilipko <plilipkoyana@gmail.com>
2023-09-13 16:19:27 +0300

    Lesson7

---
+++ Lesson/Lesson/Macuv.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson
+{
+    public class Macuv
+    {
+        static void Swap(ref int e1, ref int e2)
+        {
+            var temp = e1;
+            e1 = e2;
+            e2 = temp;
+        }
+
+        public enum SortAlgorithmType
+        {
+            Selection,
+            Bubble,
+            Insertion
+        }
+        public static int[] arr = { 3, 1, 2 };
+        public static int[] Sort(int[] arr, SortAlgorithmType sortAlgorithmType)
+        {
+            //Selection
+            for (var i = 0; i < arr.Length - 1; i++)
+            {
+                var minKey = i;
+                for (var j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minKey])
+                    {
+                        minKey = j;
+                    }
+                }
+
+                var temp = arr[i];
+                arr[i] = arr[minKey];
+                arr[minKey] = temp;
+
+            }
+
+            //Bubble 
+            if (sortAlgorithmType == SortAlgorithmType.Bubble)
+             {
+                for (int a = 0; a <= arr.Length - 2; a++)
+                {
+                    int x;
+
+                    for (int i = 0; i <= arr.Length - 2; i++)
+                    {
+                        if (arr[i] > arr[i + 1])
+                        {
+                            x = arr[i + 1];
+                            arr[i + 1] = arr[i];
+                            arr[i] = x;
+                        }
+                    }
+                }
+            }
+            //Insertion
+            if (sortAlgorithmType == SortAlgorithmType.Insertion)
+            {
+                for (var i = 1; i < arr.Length; i++)
+                {
+                    var key = arr[i];
+                    var j = i;
+                    while ((j > 1) && (arr[j - 1] > key))
+                    {
+                        Swap(ref arr[j - 1], ref arr[j]);
+                        j--;
+                    }
+                    arr[j] = key;
+                }
+            }
+            return arr;
+        }
+
+    }
+}
---
+++ Lesson/Lesson/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Lesson;
+using static Lesson.Macuv;
+
+var arrSelecion = Macuv.Sort(arr, SortAlgorithmType.Selection);
+Console.WriteLine("\n" + "Sorted array :");
+foreach (int bubble in arrSelecion)
+Console.Write(bubble + " ");
+
+var arrBubble = Macuv.Sort(arr, SortAlgorithmType.Bubble);
+Console.WriteLine("\n" + "Sorted array :");
+foreach (int bubble in arrBubble)
+Console.Write(bubble + " ");
+
+var arrInsertion = Macuv.Sort(arr, SortAlgorithmType.Insertion);
+Console.WriteLine("\n" + "Sorted array :");
+foreach (int bubble in arrInsertion)
+Console.Write(bubble + " ");
