08abbc39e86e5e90895450f0ef89b79b5c3fa840
YanaPrilipko <plilipkoyana@gmail.com>
2023-09-28 09:41:42 +0300

    Lesson 13

---
+++ Lesson/ConsoleApp2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+
+static Shape ReadShape()
+{
+    Console.WriteLine("Choose a shape type: ");
+    Console.WriteLine("1. Triangle");
+    Console.WriteLine("2. Right Triangle");
+
+Read_Input:
+    switch (int.Parse(Console.ReadLine()))
+    {
+        case 1:
+            Console.Write("Enter triangle side length: ");
+            float side = float.Parse(Console.ReadLine());
+            Console.Write("Enter triangle height: ");
+            float height = float.Parse(Console.ReadLine());
+            return new Triangle(side, height);
+        case 2:
+            Console.Write("Enter right triangle side length: ");
+            float rightTriangleSide = float.Parse(Console.ReadLine());
+            Console.Write("Enter right triangle height: ");
+            float rightTriangleHeight = float.Parse(Console.ReadLine());
+            Console.Write("Enter right triangle hypotenuse length: ");
+            float rightTriangleHypotenuse = float.Parse(Console.ReadLine());
+            return new RightTriangle(rightTriangleSide, rightTriangleHeight, rightTriangleHypotenuse);
+        default:
+            Console.Write("Incorrect shape type. Choose again: ");
+            goto Read_Input;
+    }
+}
+ ReadShape();
+static void Main(string[] args)
+{
+    Console.Write("Enter count of shapes: ");
+    int count = int.Parse(Console.ReadLine());
+
+    Shape[] shapes = new Shape[count];
+    for (int i = 0; i < count; i++)
+    {
+        shapes[i] = ReadShape();
+    }
+
+    float sum_area = 0;
+    float sum_perimeters = 0;
+    for (int i = 0; i < count; ++i)
+    {
+        sum_area += shapes[i].Area;
+        sum_perimeters += shapes[i].Perimeter;
+    }
+
+    Console.WriteLine($"Total perimeter is {sum_perimeters}");
+    Console.WriteLine($"Total area is {sum_area}");
+}
+public class Shape
+{
+    public Shape(string name)
+    {
+        _name = name;
+    }
+    private string _name = string.Empty;
+    public string Name => _name;
+    public virtual float Perimeter => 1;
+    public virtual float Area => 1;
+    public virtual float Hypotenuse => 1;
+}
+public class Triangle : Shape
+{
+    public Triangle(float side, float height)
+        : base(nameof(Triangle))
+    {
+        Side = side;
+        Height = height;
+    }
+    public float Side { get; }
+    public float Height { get; }
+    public override float Perimeter => 3 * Side;
+    public override float Area => 0.5f * (Side * Height);
+}
+public class RightTriangle : Shape
+{
+    public RightTriangle(float side, float height, float hypotenuse)
+        : base(nameof(RightTriangle))
+    {
+        Side = side;
+        Height = height;
+        Hypotenuse = hypotenuse;
+    }
+    public float Side { get; }
+    public float Height { get; }
+    public override float Hypotenuse { get; }
+    public override float Perimeter => Side + Height + Hypotenuse;
+    public override float Area => 0.5f * (Side * Height);
+}
+
+
--- Lesson/Lesson13/Program.cs
+++ Lesson/Lesson13/Program.cs
@@ -10,16 +10,11 @@ public class Shape
 
     private string _name = string.Empty;
     public string Name => _name;
-
+    public virtual float RadiusСircles => 0;
     public virtual float Perimeter => 0;
-
     public virtual float Area => 0;
-
-    public override string ToString()
-        => $"{_name} has an area of {Area} sq. units and perimeter of {Perimeter} units";
 }
 
-// Homework: implement generic triangle class
 public class Triangle : Shape
 {
     public Triangle(float side, float height)
@@ -28,110 +23,38 @@ public class Triangle : Shape
         Side = side;//cторона
         Height = height;//висота
     }
-    public float Side { get; }
-    public float Height { get; }
+    public  float Side { get; }
+    public  float Height { get; }
     public override float Perimeter => 3 * Side;
     public override float Area => (Height * Side) / 2;
 }
 
-public class RightTriangle : Shape //прямоугольный треугольник
+public class RightTriangle : Triangle //прямоугольный треугольник
 {
     public RightTriangle(float side, float height, float hypothesis) 
-            : base(nameof(RightTriangle))
+            : base(side, height)
     {
         Side = side;
         Height = height;
         Hypothesis = hypothesis;
     }
-    public float Side { get; }
-    public float Height { get; }
-    public virtual float Hypothesis { get; }
+    public  float Side { get; }
+    public  float Height { get; }
+    public float Hypothesis { get; }
+    public override float RadiusСircles => (1 / 2) * Hypothesis;
     public override float Perimeter => Side + Height + Hypothesis;
     public override float Area => (Height * Side) / 2;
 }
 
-
-public class Circle : Shape
-{
-    public Circle(float radius)
-        : base(nameof(Circle))
-    {
-        Radius = radius;
-    }
-
-    public float Radius { get; }
-
-    public override float Perimeter => 2 * MathF.PI * Radius;
-
-    public override float Area => MathF.PI * Radius * Radius;
-}
-
-public class Rectangle : Shape
-{
-    public Rectangle(float width, float height)
-        : base(nameof(Rectangle))
-    {
-        Width = width;
-        Height = height;
-    }
-    public float Width { get; }
-
-    public float Height { get; }
-
-    public override float Area => Width * Height;
-
-    public override float Perimeter => 2 * (Width + Height);
-}
-
 internal class Program
 {
-    static void SubTask1()
-    {
-        Shape some_circle = new Circle(3);
-        Shape some_rect = new Rectangle(4, 5);
-
-        Circle circle = (Circle)some_circle; // type casting - приведення типів даних
-        Rectangle rect = some_rect as Rectangle;
-
-        if (some_circle is Circle)
-        {
-            Console.WriteLine("Circle is a circle");
-        }
-
-        if (some_circle is Rectangle)
-        {
-            Console.WriteLine("Circle is a rectangle");
-        }
-
-        if (some_circle is DateTime)
-        {
-            Console.WriteLine("Circle is a datetime");
-        }
-
-        if (some_circle is object)
-        {
-            Console.WriteLine("Circle is 100% object");
-        }
-
-        if (some_circle is Shape)
-        {
-            Console.WriteLine("Circle is definetly a shape");
-        }
-
-        Console.WriteLine(circle.Radius);
-        Console.WriteLine(rect.Width);
-        Console.WriteLine(rect.Height);
-
-        Console.WriteLine($"Circle has a perimeter of {some_circle.Perimeter} and area of {some_circle.Area} sqare unit.");
-        Console.WriteLine($"Rectangle has a perimeter of {some_rect.Perimeter} and area of {some_rect.Area} sqare unit.");
-    }
-
     static void Main(string[] args)
     {
         Console.Write("Enter count of shapes: ");
         int count = int.Parse(Console.ReadLine());
 
         Shape[] shapes = new Shape[count];
+
         for (int i = 0; i < count; i++)
         {
             shapes[i] = ReadShape();
@@ -139,54 +62,43 @@ internal class Program
 
         float sum_area = 0;
         float sum_perimeters = 0;
+        float sum_radiyc = 0;
+
         for (int i = 0; i < count; ++i)
         {
             sum_area += shapes[i].Area;
             sum_perimeters += shapes[i].Perimeter;
+            sum_radiyc += shapes[i].RadiusСircles;
         }
 
-        Console.WriteLine($"Total perimeter is {sum_perimeters}");
         Console.WriteLine($"Total area is {sum_area}");
+        Console.WriteLine($"Total perimeter is {sum_perimeters}");
+        Console.WriteLine($"Total radiyc is {sum_radiyc}");
     }
 
     static Shape ReadShape()
     {
         Console.WriteLine("Choose a shape type: ");
-        Console.WriteLine("1. Circle");
-        Console.WriteLine("2. Rectangle");
-        Console.WriteLine("3. Triangle");
-        Console.WriteLine("4. RightTriangle");
+        Console.WriteLine("1. Triangle");
+        Console.WriteLine("2. RightTriangle");
 
     Read_Input:
         switch (int.Parse(Console.ReadLine()))
         {
             case 1:
-                Console.Write("Enter circle radius: ");
-                float radius = float.Parse(Console.ReadLine());
-                return new Circle(radius);
-
-            case 2:
-                Console.Write("Enter rectangle width: ");
-                float width = float.Parse(Console.ReadLine());
-                Console.Write("Enter rectangle height: ");
-                float height = float.Parse(Console.ReadLine());
-                return new Rectangle(width, height);
-            case 3:
                 Console.Write("Enter triangle side: ");
                 float side = float.Parse(Console.ReadLine());
                 Console.Write("Enter triangle height: ");
                 float height1 = float.Parse(Console.ReadLine());
                 return new Triangle(side, height1);
-            case 4:
+            case 2:
                 Console.Write("Enter righttriangle side: ");
                 float sideR = float.Parse(Console.ReadLine());
                 Console.Write("Enter righttriangle height: ");
                 float heightH = float.Parse(Console.ReadLine());
-                Console.Write("Enter righttriangle height: ");
+                Console.Write("Enter righttriangle hypothesis: ");
                 float hypothesis = float.Parse(Console.ReadLine());
                 return new RightTriangle(sideR, heightH, hypothesis);
-
-
             default:
                 Console.Write("Incorrect shape type. Choose again: ");
                 goto Read_Input;
@@ -194,3 +106,26 @@ internal class Program
     }
 }
 
+
+
+/*class Program
+{
+    static void Main1()
+    {
+        int width = 10;  
+        int height = 5;  
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+}
+*/
